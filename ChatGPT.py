f50f8c20521846b2b2377b29f6690201b7bc5c09
Linden Morgan <lmorgan2008@icloud.com>
2024-01-19 12:02:15 -0500

    Set up a function to filter query response to only payload

--- ChatGPT.py
+++ ChatGPT.py
@@ -22,15 +22,28 @@ while True:  # Use 'True' for an infinite loop
     database_response = qclient.search(
         collection_name="test_collection4", query_vector=embedded_query, limit=5
     )
-
-    messages = [{"role": "system", "content": "You are designed to assist Proctor Students."}]
     
-    messages.append({"role": "user", "content": str(database_response)})
+    def filtering(response):
+        split_response = str(response).split(":")
+        split_response = str(split_response[1]).split("}")
+        return split_response[0]
+    
+    
+    filtered = ["1","2","3","4","5"]
+
+    for i in range(5):
+        filtered[i] = filtering(str(database_response[i]))
+        print(str(i+1)+".", filtered[i], "\n")
+
+
+    messages = [{"role": "system", "content": "You are an assistent designed to answer questions about Proctor Academy. Do not halucinate responses. Make sure all responses look natural, no Answer: or Query:. DO NOT include information irrelevent to the question, even if it is given to you."}]
+    
+    messages.append({"role": "user", "content": str(filtered)})
 
     chat = oclient.chat.completions.create(
         model="gpt-3.5-turbo", messages=messages
     )
       
     reply = chat.choices[0].message.content
-    print(f"ChatGPT: {reply}")
+    print(f"ProctorPal: {reply}")
     messages.append({"role": "assistant", "content": reply})
