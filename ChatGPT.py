c55ded41e16a65303c0a274b9c3a078467bfaa9a
Linden Morgan <lmorgan2008@icloud.com>
2024-01-22 23:49:08 -0500

    added comments and worked on json implimenation

--- ChatGPT.py
+++ ChatGPT.py
@@ -4,58 +4,56 @@ from Hidden import YOUR_API_KEY
 from qdrant_client import QdrantClient
 from langchain_openai import OpenAIEmbeddings
 from openai import OpenAI
+import json
 
-
-oclient = OpenAI(api_key=YOUR_API_KEY)
 
 # Initialize clients
 embeddings_model = OpenAIEmbeddings(openai_api_key=YOUR_API_KEY)
+oclient = OpenAI(api_key=YOUR_API_KEY)
 qclient = QdrantClient("localhost", port=6333)
 
-filtered = ["1","2","3","4","5"]
-    
-history = ["1","2","3"]
 
-while True:  # Use 'True' for an infinite loop
+# Initilize lists
+history = ["1","2","3"]
+parsed = ["1","2","3","4","5"]
+
+# Main loop
+while True:
     query = input("User: ")
     
-    if query.lower() == "stop":  # Added lower() to handle different case inputs
+    # Break case
+    if query.lower() == "stop":  
         break
 
+    # Querys database
     embedded_query = embeddings_model.embed_query(query)
     
-    database_response = qclient.search(
+    # Get database output
+    database_response = qclient.search( 
         collection_name="test_collection4", query_vector=embedded_query, limit=5
     )
-
-
-    
-    def filtering(response):
-        split_response = str(response).split(":")
-        split_response = str(split_response[1]).split("}")
-        print(str(i+1)+".", split_response, "\n")
-        return split_response[0].replace("\n", "")
-
-
+    print(type(database_response))
+    # Parses JSON response from database
     for i in range(5):
-        filtered[i] = filtering(str(database_response[i]))
-    
+        parsed[i] = json.loads(str(database_response[i]))
+        parsed[i] = parsed[i]["payload"]['input']
 
+    # Gives ChatGPT api input
     messages = [{"role": "system", "content": "You are an assistent designed to answer questions about Proctor Academy."}]
-    
-    messages.append({"role": "user", "content": str(filtered)})
-    messages.append({"role": "user", "content": "Here is the chat history: " + str(history) + " Refer to this information is the user asks follow up questions."})
+    messages.append({"role": "user", "content": str(parsed)})
+    messages.append({"chat_history": "Here is the chat history: " + str(history) + " Refer to this information is the user asks follow up questions."})
 
     chat = oclient.chat.completions.create(
         model="gpt-3.5-turbo", messages=messages
     )
-      
+
+    # Gets ChatGPT api response
     reply = chat.choices[0].message.content
     print(f"ProctorPal: {reply}")
-    messages.append({"role": "assistant", "content": reply})
 
     print(messages)
 
+    # Resets history list
     for i in range(2):
         
         history[0] = ("User input", i+1, "question[s] ago:", query , "Your response", i+1, "question[s] ago:", reply )
