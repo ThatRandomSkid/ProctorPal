e4b0148117be9d76246b4443da3927187abbff7e
Linden Morgan <lmorgan2008@icloud.com>
2024-01-24 18:03:58 -0500

    Got rudamertary localhosting throught Streamlit working

--- ProctorPal.py
+++ ProctorPal.py
@@ -1,11 +1,11 @@
 import os
-import openai
 from Hidden import YOUR_API_KEY
 from qdrant_client import QdrantClient
 from langchain_openai import OpenAIEmbeddings
 from openai import OpenAI
 import json
 import streamlit as st
+import time
 
 
 # Initialize clients
@@ -13,53 +13,32 @@ embeddings_model = OpenAIEmbeddings(openai_api_key=YOUR_API_KEY)
 oclient = OpenAI(api_key=YOUR_API_KEY)
 qclient = QdrantClient("localhost", port=6333)
 
-# Initilize lists
-history = ["1","2","3"]
+# Initilize lists/variables
 parsed = ["1","2","3","4","5"]
 
+# Gets user input from site 
+query = st.text_input("User: ")
 
-# Main loop
-while True:
-    query = st.text_input("User: ")
+# Holds program until user enters text
+while query == '':
+    time.sleep(0.1)
+
+# Querys database
+embedded_query = embeddings_model.embed_query(query)
     
-    # Break case
-    if query.lower() == "stop":  
-        break
+# Get database output
+database_response = qclient.search( 
+    collection_name="test_collection4", query_vector=embedded_query, limit=5
+)
 
-    # Querys database
-    embedded_query = embeddings_model.embed_query(query)
-    
-    # Get database output
-    database_response = qclient.search( 
-        collection_name="test_collection4", query_vector=embedded_query, limit=5
-    )
+# Gives ChatGPT api input
+messages = [{"role": "system", "content": "You are an assistent designed to answer questions about Proctor Academy. Here is the user question: " + query}]
+messages.append({"role": "user", "content": str(database_response)})
 
-    # Parses JSON response from database
-    """for i in range(5):
-        parsed[i] = json.loads(database_response[i])
-        parsed[i] = parsed[i]["payload"]['input']"""
+chat = oclient.chat.completions.create(
+    model="gpt-3.5-turbo", messages=messages
+)
 
-    # Gives ChatGPT api input
-    messages = [{"role": "system", "content": "You are an assistent designed to answer questions about Proctor Academy."}]
-    messages.append({"role": "user", "content": str(database_response)})
-    messages.append({"role": "system", "content": "Here is the chat history: " + str(history) + " Refer to this information is the user asks follow up questions."})
-
-    chat = oclient.chat.completions.create(
-        model="gpt-3.5-turbo", messages=messages
-    )
-
-    # Gets ChatGPT api response
-    reply = chat.choices[0].message.content
-    print(f"ProctorPal: {reply}")
-
-    st.write(messages)
-
-    # Resets history list
-    for i in range(2):
-        
-        history[0] = ("User input", i+1, "question[s] ago:", query , "Your response", i+1, "question[s] ago:", reply )
-
-        if i < 2:
-            history[i+1] = history[i]
-        else:
-            break
+# Gets ChatGPT api response
+reply = chat.choices[0].message.content
+st.write(f"ProctorPal: {reply}")
