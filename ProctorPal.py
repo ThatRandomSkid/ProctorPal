f3f3ef96459b7aa2fdd11a4bc6388e8f9700746d
Linden Morgan <lmorgan2008@icloud.com>
2024-01-24 17:07:36 -0500

    worked on Streamlit front end and decluttered

--- ProctorPal.py
+++ ProctorPal.py
@@ -5,6 +5,7 @@ from qdrant_client import QdrantClient
 from langchain_openai import OpenAIEmbeddings
 from openai import OpenAI
 import json
+import streamlit as st
 
 
 # Initialize clients
@@ -12,14 +13,14 @@ embeddings_model = OpenAIEmbeddings(openai_api_key=YOUR_API_KEY)
 oclient = OpenAI(api_key=YOUR_API_KEY)
 qclient = QdrantClient("localhost", port=6333)
 
-
 # Initilize lists
 history = ["1","2","3"]
 parsed = ["1","2","3","4","5"]
 
+
 # Main loop
 while True:
-    query = input("User: ")
+    query = st.text_input("User: ")
     
     # Break case
     if query.lower() == "stop":  
@@ -32,16 +33,16 @@ while True:
     database_response = qclient.search( 
         collection_name="test_collection4", query_vector=embedded_query, limit=5
     )
-    print(type(database_response))
+
     # Parses JSON response from database
-    for i in range(5):
-        parsed[i] = json.loads(str(database_response[i]))
-        parsed[i] = parsed[i]["payload"]['input']
+    """for i in range(5):
+        parsed[i] = json.loads(database_response[i])
+        parsed[i] = parsed[i]["payload"]['input']"""
 
     # Gives ChatGPT api input
     messages = [{"role": "system", "content": "You are an assistent designed to answer questions about Proctor Academy."}]
-    messages.append({"role": "user", "content": str(parsed)})
-    messages.append({"chat_history": "Here is the chat history: " + str(history) + " Refer to this information is the user asks follow up questions."})
+    messages.append({"role": "user", "content": str(database_response)})
+    messages.append({"role": "system", "content": "Here is the chat history: " + str(history) + " Refer to this information is the user asks follow up questions."})
 
     chat = oclient.chat.completions.create(
         model="gpt-3.5-turbo", messages=messages
@@ -51,7 +52,7 @@ while True:
     reply = chat.choices[0].message.content
     print(f"ProctorPal: {reply}")
 
-    print(messages)
+    st.write(messages)
 
     # Resets history list
     for i in range(2):
