9b09a2134dbe08b6a435d6e5a6fddcb2c985e834
Linden Morgan <lmorgan2008@icloud.com>
2024-02-04 21:20:53 -0500

    added account system and worked on chat history implimentaion

--- ProctorPal.py
+++ ProctorPal.py
@@ -5,6 +5,7 @@ from openai import OpenAI
 import streamlit as st
 import time
 from dotenv import load_dotenv
+import json
 
 # Editable states
 response_num = 7
@@ -39,6 +40,7 @@ if "assistant_history" not in st.session_state:
 if "adequacy" not in st.session_state:
     st.session_state.adequacy = None
 
+
 # Webpage design
 #primaryColor="#F63366"
 #backgroundColor="#FFFFFF"
@@ -49,15 +51,37 @@ if "adequacy" not in st.session_state:
 st.title("ProctorPal (beta)")
 one, two, three, four, five, six, seven, eight, nine, ten, eleven, twelve = st.columns(12)
 sidebar_one, sidebar_two, sidebar_three, sidebar_four, sidebar_five = st.sidebar.columns(5)
+history_tab, beta_tab, sign_up_tab, login_tab, about_tab = st.sidebar.tabs(["Chat History","Beta","Sign Up", "Login", "About"])
 
 # Gets beta key form input
-beta_key = st.sidebar.text_input("Your super secret beta tester key:")
+beta_key = beta_tab.text_input("Your super secret beta tester key:")
+
+# Account create
+new_username = sign_up_tab.text_input("Username:")
+new_password = sign_up_tab.text_input("Password:")
+
+if sign_up_tab.button("Create account"):
+    open("Accounts.json", 'w').write(json.dumps(({new_username: {'Username': new_username, 'Password': new_password, 'Number of chats': 0}}), indent=4))
+
+# Login
+username = login_tab.text_input("Username: ")
+password = login_tab.text_input("Password: ")
+
+if login_tab.button("Login"):
+    data = json.load(open("Accounts.json", 'r'))
+    if username in data and password == data[username]["Password"]:
+        st.write(f"Welcome, {username}.")
+        user = username
+    elif username not in data:
+        st.write(f"No username {username} in system.")
+    elif password != data[username]["Password"]:
+        st.write("Password is incorrect.")
+    else:
+        st.write("Username or password are incorrect.")
 
 # Provides link to GitHub in the sidebar
-for i in range(31):
-    st.sidebar.write('')
-st.sidebar.write("Developed by Linden Morgan")
-st.sidebar.write("If you're interested in the code for this project, you can check it out here: https://github.com/ThatRandomSkid/ProctorPal")
+about_tab.write("Developed by Linden Morgan")
+about_tab.write("If you're interested in the code for this project, you can check it out here: https://github.com/ThatRandomSkid/ProctorPal")
 
 # Determine user
 if beta_key == linden_key:
@@ -90,7 +114,34 @@ elif beta_key == linden_admin_key or beta_key == guest_admin_key:
 else:
     auth_state = False
     query = None
-    
+
+# Gets chat history from Accounts file
+data = json.load("Accounts.json")
+chats = data[username]["Number of chats"]
+
+if history_tab.button("New Chat"):
+    chats = chats + 1
+
+# Sets session states for historic chats
+for i in range(chats-1):
+    if f"user_history{i}" not in st.session_state:
+        st.session_state.user_history = [data[username]["Chat History"][i]["user_history"]]
+    if f"assistant_history{i}" not in st.session_state:
+        st.session_state.assistant_history = [data[username]["Chat History"][i]["assistant_history"]]
+
+
+# Displays chat history
+for i in range(chats):
+    if history_tab.button(f"Chat {chats}"): # Replace with gpt-3.5 generated chat names
+        for i in range(len(st.session_state.user_history)):
+            with st.chat_message("user"):
+                st.write(st.session_state.user_history[i])
+            if len(st.session_state.assistant_history) != 0 and i < len(st.session_state.assistant_history):
+                with st.chat_message("assistant"):
+                    st.write(st.session_state.assistant_history[i-1])
+        
+        
+
 # Gets user input from site 
 if auth_state == True :
     query = st.chat_input(user + ":")
@@ -152,13 +203,8 @@ elif gpt_version == 4:
 chat = oclient.chat.completions.create(
     model=gpt_version, messages=messages, max_tokens=gpt_tokens
 )
-reply = chat.choices[0].message.content
-
-# Updates logs
-if admin == False and user != "Linden (user)":
-    f1 = open("Logs.txt", "a")
-    f1.write("\n\n" + user + ": " + query + "\n" + "ProctorPal: " + reply)
-    f1.close()
+with st.spinner():
+    reply = chat.choices[0].message.content
 
 # Logs ChatGPT response in history
 st.session_state.assistant_history.append(reply)
@@ -167,13 +213,14 @@ st.session_state.assistant_history.append(reply)
 with st.chat_message("assistant"):
     st.write(st.session_state.assistant_history[-1])
 
+# Updates logs
+if admin == False and user != "Linden (user)":
+    f1 = open("Logs.txt", "a")
+    f1.write("\n\n" + user + ": " + query + "\n" + "ProctorPal: " + reply)
+    f1.close()
+
 # Buttons allowing user rate responses
 if st.button("Response was inadequate"): 
-    st.session_state.adequacy = False
-
-# Checks for insufficient data
-if st.session_state.adequacy is False:
     f2 = open("Unanswered_Questions.txt", "a")
-    f2.write("\n\n"+ st.session_state.assistant_history[-1])
+    f2.write("\n\n" + user + ": " + query + "\n" + "ProctorPal: " + reply)
     f2.close()
-    st.session_state.adequacy = None
